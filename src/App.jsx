46b6c4010a85a865655e2f1e51332b04b3d3749a
Realrickkel <77985377+Realrickkel@users.noreply.github.com>
2024-11-12 15:04:02 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Experience from "./Components/Experience"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+
+  return (
+    <main>
+    <Navbar/>
+    <Hero/>
+    <Experience/>
+    </main>
+  )
+}
+
+export default App
